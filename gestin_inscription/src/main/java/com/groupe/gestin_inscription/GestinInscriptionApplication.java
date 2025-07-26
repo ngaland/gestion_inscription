27af1dfef4b2158788342ba312db0d4a30303afd
ngaland <ngaland093@gmail.com>
2025-07-26 10:40:59 +0100

    Premier commit

---
+++ gestin_inscription/src/main/java/com/groupe/gestin_inscription/GestinInscriptionApplication.java
@@ -0,0 +1,13 @@
+package com.groupe.gestin_inscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestinInscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestinInscriptionApplication.class, args);
+	}
+
+}
