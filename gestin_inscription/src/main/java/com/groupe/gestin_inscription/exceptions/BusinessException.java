bfbfbe791fd4947edeb3082083a0f482962b53eb
hels <m7hels@gmail.com>
2025-07-28 04:34:33 +0100

    added Exceptions and global validator

---
+++ gestin_inscription/src/main/java/com/groupe/gestin_inscription/exceptions/BusinessException.java
@@ -0,0 +1,10 @@
+package com.groupe.gestin_inscription.exceptions;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ gestin_inscription/src/main/java/com/groupe/gestin_inscription/exceptions/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.groupe.gestin_inscription.exceptions;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
